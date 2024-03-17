15a3b28c5828e495bcf66a51baa8de2670a40ad2
Maneeshkkt <141389961+Maneeshkkt@users.noreply.github.com>
2024-03-17 18:42:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+navlist=document.querySelector('.nav-list')
+rightnav=document.querySelector('.rightnav')
+
+
+
+burger.addEventListener('click', ()=>{
+    rightnav.classList.toggle('v-class-resp');
+    navlist.classList.toggle('v-class-resp');
+    navbar.classList.toggle('hnav-resp');
+
+
+})
+
+function sentToWhatsapp(){
+    let number="+918957118936";
+
+    let name=document.getElementById('name2').value;
+    let email=document.getElementById('email2').value;
+    let filep=document.getElementById('file2').value;
+    let meesage=document.getElementById('textarea2').value;
+
+    var url="https://wa.me/" + number + "?text="
+    +"Name : " +name+ "%0a"
+    +"Email : " +email+ "%0a"
+    +"Plan : " +filep+ "%0a"
+    +"message : " +meesage+ "%0a%0a";
+
+    window.open(url, '_blank').focus();
+
+}
